1e2afa2b241c3751a9cbc6799caff51e64a9d8a4
Yusuf Abdulrahman <yusufabdulrahman05@gmail.com>
2023-11-18 12:44:09 -0500

    up

---
+++ Back-End ass 1/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = 12;
+
+$b = 5;
+
+$sum = $a + $b;
+$mult = $a * $b;
+$div = $a / $b;
+$mod = $a % $b;
+
+
+echo 'The value of a is ' . $a;
+echo '<br>The value of b is ' . $b;
+echo '<br>The sum is ' . $sum;
+echo '<br>The multiplication is ' . $mult;
+echo '<br>The Division is ' . $div;
+echo '<br>The Modulus is ' . $mod;
+
+
+?>
