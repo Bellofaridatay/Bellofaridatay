0fcb5a9f3da291939a69212b09ef525976b6f576
Yusuf Abdulrahman <yusufabdulrahman05@gmail.com>
2023-12-03 06:19:20 -0500

    up

---
+++ Back-End ass 1/option.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Select the Geometry to Compute</h2>
+
+<a href="sq.php">Square</a>
+
+
+
+
+<!--
+
+<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+
+  <label for="fname">Enter the length of the Square:</label><br>
+  <input type="text" name="len" value="<?php if($per != -1)echo $per; ?>"><br>
+
+  <label for="fname">The Area of a Square is:</label><br>
+  <input type="text" value="<?php if($area != -1)echo $area; ?>"><br>
+
+  <label for="fname">The Perimeter of a square is:</label><br>
+  <input type="text" value="<?php if($persq != -1)echo $persq; ?>"><br>
+
+  <input type="submit" value="Select"><br>
+
+</form> 
+
+-->
+
+
+</body>
+</html>
--- Back-End ass 1/sq.php
+++ Back-End ass 1/sq.php
@@ -1,16 +1,33 @@
 <?php
 
+$pi = 3.142;
 
-$area = $per = -1;
-$persq = -1;
+
+$area = $len = $persq = "";
+$areac = $circum = $rad = "";
 
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-$per = test($_POST['len']);
+$len = test($_POST['len']);
+$rad = test($_POST['rad']);
 
-$area = $per ** 2;
-$persq = $per * 4;
+if($rad == "")$rad = $areac = $circum = "";
+
+else{
+  $areac = $pi * ($rad ** 2);
+  $circum = 2 * $pi * $rad;
+}
+
+if($len == "")$len = $persq = $areasq = "";
+
+
+  else{
+    $areasq =$len ** 2;
+    $persq = 2 * $len;
+  }
+
+
 
 //print("The area of the square is $area cm^2");
 
@@ -22,6 +39,12 @@ function test($data) {
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
+  }
+
+
+  function brfxn($count){
+
+    for ($i = 1; $i <= $count; $i++)echo "<br>";
   }
 
 
@@ -37,13 +60,23 @@ function test($data) {
 <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
 
   <label for="fname">Enter the length of the Square:</label><br>
-  <input type="text" name="len" value="<?php if($per != -1)echo $per; ?>"><br>
+  <input type="text" name="len" value="<?php print $len; ?>"><br>
 
   <label for="fname">The Area of a Square is:</label><br>
-  <input type="text" value="<?php if($area != -1)echo $area; ?>"><br>
+  <input type="text" value="<?php print $areasq; ?>"><br>
 
   <label for="fname">The Perimeter of a square is:</label><br>
-  <input type="text" value="<?php if($persq != -1)echo $persq; ?>"><br>
+  <input type="text" value="<?php print $persq; ?>"><br><br>
+  
+
+  <label for="fname">Enter the Radius of a circle:</label><br>
+  <input type="text" name="rad" value="<?php echo $rad; ?>"><br>
+
+  <label for="fname">The Area of a circle is:</label><br>
+  <input type="text" value="<?php echo $areac; ?>"><br>
+
+  <label for="fname">The Circumference of a circle is:</label><br>
+  <input type="text" value="<?php echo $circum; ?>"><br><br>
 
   <input type="submit" value="Calculate"><br>
 
